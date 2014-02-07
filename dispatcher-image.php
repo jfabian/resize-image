1310b37f930ac6e5273d45af54c6d957a8027749
Jesus Fabian <jfabian.pe@gmail.com>
2014-02-07 09:22:19 -0500

    consulta la existencia de la imagen antes de redimensionar

--- dispatcher-image.php
+++ dispatcher-image.php
@@ -6,12 +6,16 @@ $newImage = end($nameImage);
 list($width, $height) = explode('__', $newImage);
 if ((int)$width > 0 && (int)$height > 0) {
         $pathOriginalImage = str_replace($width."__".$height."__", "", $pathNewImage);
-        $command = "convert $pathOriginalImage -resize $width"."x"."$height"."! $pathNewImage";
-        exec($command);
+	if (file_exists($pathOriginalImage)) {
+	        $command = "convert $pathOriginalImage -resize $width"."x"."$height"."! $pathNewImage";
+        	exec($command);
+	}
 } elseif ($width > 0) {
         $pathOriginalImage = str_replace($width."__", "", $pathNewImage);
-        $command = "convert $pathOriginalImage -resize $width $pathNewImage";
-        exec($command);
+	if (file_exists($pathOriginalImage)) {
+	        $command = "convert $pathOriginalImage -resize $width $pathNewImage";
+        	exec($command);
+	}
 }
 if (file_exists($pathNewImage)) {
         header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
